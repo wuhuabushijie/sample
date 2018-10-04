376319e1ef56eb8996f2c3e4ad369b338477b20b
wuhua <214148402@qq.com>
2018-10-04 13:36:12 +0800

    test

--- chapter08/property_test.py
+++ chapter08/property_test.py
@@ -4,11 +4,21 @@ class User:
     def __init__(self,name,birthday):
         self.name = name
         self.birthday=birthday
+        self._age=None
 
     @property
     def age(self):
+        if self._age:
+            return self._age
         return datetime.now().year - self.birthday.year
+
+    @age.setter
+    def age(self,value):
+        self._age=value
 
 if __name__ == "__main__":
     user = User("bob",date(1989,11,21))
+    print(user.age)
+    user._age=45
+    print(user._age)
     print(user.age)
