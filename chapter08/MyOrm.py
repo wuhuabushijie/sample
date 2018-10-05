e150ffe243e677741d4d5ce0a9deded6812f7bb2
wuhua <214148402@qq.com>
2018-10-06 01:11:41 +0800

    test

--- chapter08/MyOrm.py
+++ chapter08/MyOrm.py
@@ -19,6 +19,7 @@ class CharField(Field):
             raise ValueError("must be string")
         if len(value)>self.max_length:
             raise ValueError("string is too long")
+        self._value=value
 
 class IntField(Field):
     def __init__(self, db_column, min_value=None, max_value=None):
@@ -50,7 +51,42 @@ class IntField(Field):
             raise ValueError("value must between min_value and max_value")
         self._value = value
 
+class ModelMetaClass(type):
+    def __new__(cls, name, bases, attrs, **kwargs):
+        if name == "BaseModel":
+            return super().__new__(cls, name, bases, attrs, **kwargs)
+        fields={}
+        for key, value in attrs.items():
+            if isinstance(value,Field):
+                fields[key]=value
+        attrs_meta = attrs.get("Meta",None)
+        _meta = {}
+        db_table = name
+        if attrs_meta is not None:
+            table = getattr(attrs_meta,"db_table",None)
+            if table is not None:
+                db_table = table
+        _meta["db_table"]=db_table
+        attrs["_meta"]=_meta
+        attrs["fields"]=fields
+        del attrs["Meta"]
+        return super().__new__(cls,name,bases,attrs,**kwargs)
 
-class User:
-    def __init__(self):
-        pass
+class BaseModel(metaclass=ModelMetaClass):
+    def __init__(self,*args,**kwargs):
+        for key,value in kwargs.items():
+            setattr(self,key,value)
+        # return super().__init__()
+
+class User(BaseModel):
+    user_name = CharField(db_column="",max_length=10)
+    user_age = IntField(db_column="",min_value=0,max_value=100)
+
+    class Meta:
+        db_table = "user"
+
+if __name__ == "__main__":
+    user = User()
+    user.user_name="bob"
+    user.user_age=23
+    print(user.user_name,user.user_age)
